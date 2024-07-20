5d72684ebd0f9700c3cfbd353726dd42aeb26d24
manumn95 <manumnofficial.95@gmail.com>
2024-07-20 14:38:03 +0530

    ficx the bug and created new backed project

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin :'*',
+    origin :'https://chatapp-frontend-ten-jade.vercel.app',
     credentials : true
 }))
 app.use(express.json())
